035c3a0aac64dc7bfb538a165ba2f70eb493ae0f
yk3372 <yk3372@gmail.com>
2015-10-16 14:44:33 +0800

    add filter function
    Signed-off-by: yk3372 <yk3372@gmail.com>

--- color_log.py
+++ color_log.py
@@ -5,7 +5,22 @@ import re,sys,codecs
 out1 = """
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    <script>
+        function filter_it() {
+            var keyword = document.getElementById("keyword").value;
+            var patt1=new RegExp(keyword);
+            labels = document.getElementsByTagName("label");
+            for (var i=0;i<labels.length;i++) {
+                content = labels[i].innerText;
+                if (!patt1.test(content)) {
+                    labels[i].style.display = "none";
+                } else {
+                    labels[i].style.display = "block";
+                }
+            }
+        }
+    </script>
     <style>
         body{
         background-color:#C7EDCC;
@@ -42,6 +57,7 @@ out1 = """
     </style>
 </head>
 <body>
+please input keywords<input id="keyword" style="width:500px"><input type="button" onclick="filter_it();" value="filter">
 <div>"""
 out2 = """
 </div>
@@ -54,27 +70,27 @@ divContent=""
 pattern = re.compile(
     r'[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{3}\s([DWIVE])[\s\S]*')
 with codecs.open("log.html","w", 'utf-8') as h5:
-	h5.write(out1)
-	h5.write("<label>")
-	with codecs.open("log.txt","r", 'utf-8') as f:
-		for line in f:
-			match = pattern.match(line)
-			line = line.replace(' ', '&nbsp;').replace('\n', '<br/>')
-			if match:
-				type = match.group(1)
-				if type == 'I':
-					divContent = "</label><label class=\"line_I\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
-				elif type == 'D':
-					divContent = "</label><label class=\"line_D\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
-				elif type == 'V':
-					divContent = "</label><label class=\"line_V\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
-				elif type == 'W':
-					divContent = "</label><label class=\"line_W\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
-				elif type == 'E':
-					divContent = "</label><label class=\"line_E\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
-			else:
-				divContent = line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
-			h5.write(divContent)
-			id_index+=1
-	h5.write("</label>")
-	h5.write(out2)
+    h5.write(out1)
+    h5.write("<label>")
+    with codecs.open("log.txt","r", 'utf-8') as f:
+        for line in f:
+            match = pattern.match(line)
+            line = line.replace(' ', '&nbsp;').replace('\n', '<br/>')
+            if match:
+                type = match.group(1)
+                if type == 'I':
+                    divContent = "</label><label class=\"line_I\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
+                elif type == 'D':
+                    divContent = "</label><label class=\"line_D\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
+                elif type == 'V':
+                    divContent = "</label><label class=\"line_V\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
+                elif type == 'W':
+                    divContent = "</label><label class=\"line_W\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
+                elif type == 'E':
+                    divContent = "</label><label class=\"line_E\" id='index_"+str(id_index)+"'>" + line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;")
+            else:
+                divContent = line.replace('\t',"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
+            h5.write(divContent)
+            id_index+=1
+    h5.write("</label>")
+    h5.write(out2)
